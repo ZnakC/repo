91f6afc08196d9a0fb5bd2889bea5b04b6aad33b
Zhanna <zhanigorch@gmail.com>
2023-05-04 08:50:19 +0300

    + .gitignore

---
+++ v.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL,"Russian");
+	float a,b,c;
+	cout<<"Введите сторону:"<<endl;
+	cin>>a;
+	cout<<"Введите сторону:"<<endl;
+	cin>>b;
+	cout<<"Введите сторону:"<<endl;
+	cin>>c;
+if
+    ((pow(c,2)==pow(a,2)+pow(b,2))||(pow(a,2)==pow(c,2)+pow(b,2))||(pow(b,2)==pow(a,2)+pow(c,2)))
+    cout<<"Результат: Треугольник прямоугольный"<<endl;
+else 
+    cout<<"Резeльтат: Треугольник не прямоугольный"<<endl;
+return 0;
+}
